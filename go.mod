c20ad4935f1af905d55d6118fdb728165819ddd8
Shuhai Liao <shuai190060roy@gmail.com>
2023-09-22 11:04:10 +0200

    add wait for pod function

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/shuai190060/k8s_client
 go 1.19
 
 require (
+	k8s.io/api v0.28.0-alpha.3
+	k8s.io/apimachinery v0.28.0-alpha.3
+	k8s.io/client-go v0.28.0-alpha.3
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
@@ -34,9 +40,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.28.2 // indirect
-	k8s.io/apimachinery v0.28.2 // indirect
-	k8s.io/client-go v0.28.2 // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9 // indirect
 	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"os"
 
@@ -10,6 +11,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
@@ -29,6 +31,11 @@ func main() {
 	// fmt.Printf("test:%+v", client) // for debug
 
 	if deploymentlabel, err = deploy(ctx, client); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err = waitForPod(ctx, client, deploymentlabel); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -87,10 +94,43 @@ func deploy(ctx context.Context, client *kubernetes.Clientset) (map[string]strin
 	// decode := schema.Codecs.UniversalDeserializer().Decode
 	deploymentResponse, err := client.AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
 	if err != nil && errors.IsAlreadyExists(err) {
-		return map[string]string{err.Error(): deployment.Name}, nil
+		existingDeployment, getErr := client.AppsV1().Deployments("default").Get(ctx, deployment.Name, metav1.GetOptions{})
+		if getErr != nil {
+			return nil, fmt.Errorf("error fetch deployment: %s", getErr)
+		}
+		return existingDeployment.Spec.Template.Labels, nil
 		// return nil, fmt.Errorf("deployment error:%s", err)
 	} else if err != nil && !errors.IsAlreadyExists(err) {
 		return nil, fmt.Errorf("deployment error:%s", err)
 	}
 	return deploymentResponse.Spec.Template.Labels, nil
+}
+
+func waitForPod(ctx context.Context, client *kubernetes.Clientset, deploymentslabels map[string]string) error {
+	for {
+
+		validatedLabels, err := labels.ValidatedSelectorFromSet(deploymentslabels)
+		if err != nil {
+			return fmt.Errorf("ValidatedSelectorFromSet error:%s", err)
+		}
+		podList, err := client.CoreV1().Pods("default").List(ctx, metav1.ListOptions{
+			LabelSelector: validatedLabels.String(),
+		})
+		if err != nil {
+			return fmt.Errorf("pod list error: %s", err)
+		}
+
+		podsRunning := 0
+		for _, pod := range podList.Items {
+			if pod.Status.Phase == "Running" {
+				podsRunning++
+			}
+		}
+		if podsRunning > 0 && podsRunning == len(podList.Items) {
+			break
+		}
+		fmt.Printf("waiting for pdos to become ready.(running %d /%d)", podsRunning, len(podList.Items))
+		time.Sleep(5 * time.Second)
+	}
+	return nil
 }
